56cb7e7b6ee1dc5762054bac20aa783d04ba0082
Jacob <jacob_murray@hotmail.co.uk>
2018-11-13 09:53:24 +0000

    encrypted password on seed

---
+++ seed/devData/users.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+      "username": 'Jacobgodslayer',
+      "name": 'Jacob Murray',
+      "password": 'password',
+      "confirm": 'password'
+    },
+    {
+      "username": 'NickyBee',
+      "name": 'Nick Bansal',
+      "password": 'password',
+      "confirm": 'password'
+    }
+  ];
+  
--- seed/seed.js
+++ seed/seed.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const passport = require('passport')
 
 const seedDB = userData => {
-  return mongoose.connection.dropDatabase().then(() => {
-      User.insertMany(userData);
+  return mongoose.connection
+  .dropDatabase()
+  .then(() => {
+   const newUser =  userData.map(user => {
+      const { username, name, password } = user
+      return User.register(new User({ username, name }), password)
+    })
+     return Promise.all(newUser)   
   })
+  .then(user => {
+    passport.authenticate('local');
+    console.log(user);
+  })
+  .catch(error => {
+    console.log(error);
+    res.send(error);
+  });
 };
 
 module.exports = seedDB;
