44162899d9c6460619cebda0926d5ff4628e88d7
Jacob <jacob_murray@hotmail.co.uk>
2018-11-13 13:00:07 +0000

    finished passwordStrength testing

--- spec/utills.spec.js
+++ spec/utills.spec.js
@@ -5,7 +5,16 @@ describe('passwordStength', () => {
     it('When given a strong password return true', () => {
         expect(passwordStrength('Helloworld3%')).to.equal(true)
     });
-    it('When given a missing a capital letter', () => {
+    it('When given a password missing a capital letter', () => {
         expect(passwordStrength('helloworld3%').message).to.equal('please enter a uppercaseLetter in password')
+    });
+    it('When given a password missing a number', () => {
+        expect(passwordStrength('Helloworld%').message).to.equal('please enter a digit in password')
+    });
+    it('When given a password missing a special character', () => {
+        expect(passwordStrength('Helloworld3').message).to.equal('please enter a specialCharacter in password')
+    });
+    it('When given a password not log enough', () => {
+        expect(passwordStrength('Hello').message).to.equal('please enter a longerWord in password')
     });
 });
